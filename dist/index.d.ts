2b4ac1e0064f517301d4ac373f215c9c53fba468
Ivan Kalagin <lulldev@yandex.ru>
2022-04-28 15:39:35 +0300

    Update dist to 0.1.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
-import { ReactNode, ReactInstance } from 'react';
+import { ReactElement, ReactFragment, ReactPortal, ReactInstance } from 'react';
+declare type ReactNode = ReactElement | ReactFragment | ReactPortal;
 declare type NonEmptyArray<T> = [T, ...T[]];
 export default function requiredNodes(nodes: NonEmptyArray<ReactNode | ReactInstance> | NonEmptyArray<(...args: any) => JSX.Element>): (props: {
     [key: string]: any;
